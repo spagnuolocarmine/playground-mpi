af838efb717e5fa89a611aa01d9ceada1d0a4850
Carmine Spagnuolo <spagnuolocarmine@gmail.com>
2020-02-10 13:14:19 +0100

    cap 2

--- exercises/2/2.c
+++ exercises/2/2.c
@@ -16,12 +16,19 @@ int main(argc,argv) int argc; char *argv[];
 
     if (rank == 0) {
         MPI_Buffer_attach( buf, bufsize ); 
+        int time = MPI_Wtime();
         MPI_Bsend(&outmsg, 1, MPI_CHAR, 1, 1, MPI_COMM_WORLD);
-        printf("Task %d: buffered send buffered, process will block on detach.\n",rank);
+        printf("Task %d: buffered send (1) buffered , process will block on detach. Time: %1.2f\n",rank, MPI_Wtime() - time);
         MPI_Buffer_detach( &buf, &bufsize ); 
-        printf("Task %d: end buffered send.\n");
+        time = MPI_Wtime();
+        MPI_Bsend(&outmsg, 1, MPI_CHAR, 1, 1, MPI_COMM_WORLD);
+        printf("Task %d: buffered send (2), process maybe will block here. Time: %1.2f\n",rank, MPI_Wtime() - time);
         
     } else {
+        MPI_Recv(&inmsg, 1, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Stat);
+        rc = MPI_Get_count(&Stat, MPI_CHAR, &count);
+        printf("Task %d: Received %d char(s) from task %d with tag %d \n",
+           rank, count, Stat.MPI_SOURCE, Stat.MPI_TAG);
         MPI_Recv(&inmsg, 1, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Stat);
         rc = MPI_Get_count(&Stat, MPI_CHAR, &count);
         printf("Task %d: Received %d char(s) from task %d with tag %d \n",
