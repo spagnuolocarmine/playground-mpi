88e3e4173899b96e1a9a67623c6f097f42b26551
Carmine Spagnuolo <spagnuolocarmine@gmail.com>
2020-02-10 12:55:06 +0100

    cap 2

---
+++ exercises/2/2.c
@@ -0,0 +1,32 @@
+#include "mpi.h"
+#include <stdio.h>
+
+int main(argc,argv) int argc; char *argv[];
+{
+    int numtasks, rank, dest, source, rc, count;  
+    char inmsg, outmsg='x';
+    MPI_Status Stat;
+
+    int  bufsize; 
+    char *buf = malloc(bufsize); 
+   
+
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+        MPI_Buffer_attach( buf, bufsize ); 
+        MPI_Bsend(&outmsg, 1, MPI_CHAR, 1, 1, MPI_COMM_WORLD);
+        printf("Task %d: buffered send buffered, process will block on detach.\n");
+        MPI_Buffer_detach( &buf, &bufsize ); 
+         printf("Task %d: end buffered send.\n");
+        
+    } else {
+        MPI_Recv(&inmsg, 1, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Stat);
+        rc = MPI_Get_count(&Stat, MPI_CHAR, &count);
+        printf("Task %d: Received %d char(s) from task %d with tag %d \n",
+           rank, count, Stat.MPI_SOURCE, Stat.MPI_TAG);
+    }
+    MPI_Finalize();
+}
---
+++ exercises/2/2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo -e "Compiling"
+cd /project/target/2
+rm -rf out 2
+mpicc -o 1 2.c 2> err_log
+rc=$?
+if [[ $rc != 0 ]]; then
+    echo -e "Error : mpicc returned"
+    cat err_log
+    echo "TECHIO> success false"
+    rm -rf out
+    exit $rc
+fi
+echo "Compilation is OK"
+rm -rf out
+mkdir out
+rc=0
+
+echo -e "Execution ... "
+mpirun --allow-run-as-root  -mca btl sm,tcp,self -np 2 -output-filename out/out ./2
+cat out/*
+rm -rf out
+
